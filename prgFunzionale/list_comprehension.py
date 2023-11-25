23deb7517b70a062e605e0435493dc9ad28e2cd7
Manillin <chri.vwm@gmail.com>
2023-11-25 13:10:53 +0100

    list comprehension with various data types

---
+++ prgFunzionale/list_comprehension.py
@@ -0,0 +1,14 @@
+# list comprehension:
+# [exp for valie in iterble if condition]
+i = 1
+disp = [i for i in range(i, 11) if i % 2 != 0]
+lis = [i*i for i in range(i, 6)]
+
+set = {i*i for i in range(1, 6)}
+dic = {i: i*2 for i in range(i, 6)}
+
+
+print(set)
+print(lis)
+print(disp)
+print(dic)
