0126542f3a722de517e591f7a35c30f9d4d8b188
Manillin <chri.vwm@gmail.com>
2023-12-12 16:36:33 +0100

    map reduce explained + excercises solved

--- prgFunzionale/lambda2.py
+++ prgFunzionale/lambda2.py
@@ -1,8 +1,33 @@
-l = ['ciao', 'ao', 'hola', 'ciao']
+iter = [i for i in range(11)]
+print(iter)
 
-filter_ciao = filter(lambda x: x == 'ciao', l)
-print(list(filter_ciao))
+f_items = filter(lambda x: x % 2, iter)
+# spiegazione:
+'''
+filter ha il compito di selezionare item in base al loro valore di ritorno una 
+volta che gli si applica la funzione definita.
+in questo caso, i numeri dispari se messi in modulo con 2 fanno 1, che risulta 
+True, e quindi il risultato verrà messo in f_items.
+Mentre i dispari facendo %2 -> 0, risulteranno false [0 == False | 1 == True]
+'''
+
+f_list = list(f_items)
+print(f_list)
 
 
-append123 = map(lambda x: x+'123', l)
-print(list(append123))
+stringhe = ["ciao", "come", "stai", "cocco"]
+
+
+def string_filter(str):
+    if 'c' in str:
+        return True
+    return False
+
+
+str_filter = filter(string_filter, stringhe)
+str_filter2 = filter(lambda x: 'a' in x, stringhe)
+list_str = list(str_filter)
+
+print(list_str)
+print("sec: ")
+print(list(str_filter2))
--- prgFunzionale/map_reduce.py
+++ prgFunzionale/map_reduce.py
@@ -1,11 +1,40 @@
-from functools import reduce
+from functools import *
 
-i = 1
-nums = [i for i in range(i, 5)]
-print(nums)
+interi = [1, 2, 3, 4, 5]
+int = [i for i in range(1, 6)]
 
-red = reduce(lambda x, y: x+y, nums)
-print(red)
 
-setr = reduce(lambda x, y: x.union(set([y])), nums, set())
-print(setr)
+def somma(x, y):
+    return x+y
+
+# sommapart = partial(somma, y=10)
+# red = reduce(somma, int)
+# print(red)
+
+
+# creazione di un set da una lista:
+
+s_reduce = reduce(lambda x, y: x.union(set([y])), [1, 2, 3, 4], set())
+# spiegazione:
+'''
+la funzione reduce ha 3 parametri in questo caso:
+1. la funzione lambda
+2. una lista (iterabile)
+3. una funzione da chiamare sul primo elemento dell'iterabile
+
+alla prima chiamata viene invocata set() sul primo elemento della lista 
+che risulta nel set -> {1}. questo sarà la x per la prossima chiamata.
+In quanto set possiamo usare la primitiva .union() e trasformiamo in set 
+la y che presa da parte sarebbe un intero, quindi  set([y]) trasforma l'int
+in una lista di lunghezza 1 con l'intero in questione.
+A questo punto avremo {1}.union(set([2])) che risulta in {1,2}.
+procedendo con la stessa logica arriveremò all'ultima iterazione, che 
+tradotta risulta in: {1,2,3}.union(set([4])) -> {1,2,3,4}
+fine spiegazione
+'''
+
+
+'''a = [5]
+s_ = set(a)
+print(s_)'''
+print(s_reduce)
